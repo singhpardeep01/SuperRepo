b793c0285a9c2c7bbc759a262a2e0d37742f6219
singhpardeep01 <psingh@stuy.edu>
2015-12-09 23:45:25 -0500

    SuperArray new methods and comparable

--- SuperArray.java
+++ SuperArray.java
@@ -22,7 +22,7 @@ public class SuperArray{
 
     //~~~~~INSTANCE VARS~~~~~
     //underlying container, or "core" of this data structure:
-    private int[] _data;
+    private Comparable[] _data;
 
     //position of last meaningful value
     private int _lastPos;
@@ -33,7 +33,7 @@ public class SuperArray{
     //~~~~~METHODS~~~~~
     //default constructor – initializes 10-item array
     public SuperArray() {
-	_data = new int[10];
+	_data = new Comparable[10];
 	_lastPos = -1; //flag to indicate no lastpos yet
 	_size = 0;
     }
@@ -54,28 +54,28 @@ public class SuperArray{
 
     //double capacity of this SuperArray
     private void expand() {
-	int[] temp = new int[_data.length * 2];
+	Comparable[] temp = new Comparable[_data.length * 2];
 	for (int i = 0; i < _data.length; i++)
 	    temp[i] = _data[i];
 	_data = temp;
     }
 
     //accessor -- return value at specified index
-    public int get(int index) {
+    public Comparable get(int index) {
 	return _data[index];
     }
 
     //mutator -- set value at index to newVal,
     //           return old value at index
-    public int set(int index, int newVal) {
-	int temp = _data[index];
+    public Comparable set(int index, Comparable newVal) {
+	Comparable temp = _data[index];
 	_data[index] = newVal;
 	return temp;
     }
 
     // ~~~~~~~~~~~~~~ PHASE II ~~~~~~~~~~~~~~
     //adds an item after the last item
-    public void add(int newVal) {
+    public void add(Comparable newVal) {
 	if (_lastPos == _size - 1) {
 	    expand();
 	}
@@ -86,7 +86,7 @@ public class SuperArray{
     
     //inserts an item at index
     //shifts existing elements to the right
-    public void add(int index, int newVal) {
+    public void add(int index, Comparable newVal) {
 	if (index < _size) {
 	    for (int x = _lastPos + 1; x > index; x--) {
 		_data[x]= _data[x-1];
@@ -111,16 +111,38 @@ public class SuperArray{
     public int size() {
 	return _size;
     }
+
+    //searches for first occurance of Comparable input
+    public int linSearch(Comparable find) {
+	for (int x = 0; x <= _lastPos; x++) {	    
+	    if (_data[x].equals(find)) {
+		return x;
+	    }
+	}
+	return -1;
+    }
+    
+    //checks if _data is sorted
+    public boolean isSorted() {
+	for (int x = 1; x <= _lastPos; x++) {
+	    try {
+		if (_data[x].compareTo(_data[x-1]) > 0) {
+		    return true;
+		}
+	    }
+	    catch (Exception e) {}
+	}
+	return false;
+    }
     
     //main method for testing
     public static void main(String[] args) {
 	
 	SuperArray curtis = new SuperArray();
 	System.out.println("Printing empty SuperArray curtis...");
-	System.out.println(curtis);
-	    
+	System.out.println(curtis);	    
 	for (int i = 0; i < curtis._data.length; i++) {
-	    curtis.set(i, i * 2);
+	    curtis.set(i, new Binary(i * 2));
 	    curtis._size++; //necessary bc no add() method yet
 	}
 	    
@@ -141,15 +163,25 @@ public class SuperArray{
 	System.out.println("Printing empty SuperArray mayfield...");
 	System.out.println(mayfield);
 
-	mayfield.add(5);
-	mayfield.add(4);
-	mayfield.add(3);
-	mayfield.add(2);
-	mayfield.add(1);
+	mayfield.add(new Binary(5));
+	mayfield.add(new Hexadecimal(4));
+	mayfield.add(new Binary("1110"));
+	mayfield.add(new Hexadecimal("F"));
+	mayfield.add(new Rational(1,2));
 
 	System.out.println("Printing populated SuperArray mayfield...");
 	System.out.println(mayfield);
-	    
+
+	System.out.println("\nTesting linSearch() mayfield...");
+	System.out.println(mayfield.linSearch(new Binary(5)));//expects 0
+	System.out.println(mayfield.linSearch(new Hexadecimal(4)));//expects 1
+	System.out.println(mayfield.linSearch(new Binary("1110")));//expects 2
+	System.out.println(mayfield.linSearch(new Hexadecimal("F")));//expects 3
+	System.out.println(mayfield.linSearch(new Binary(0)));//expects -1
+		
+	System.out.println("\nTesting isSorted() mayfield...");
+	System.out.println(mayfield.isSorted());//expects false
+	
 	mayfield.remove(3);
 	System.out.println("Printing SuperArray mayfield post-remove...");
 	System.out.println(mayfield);
@@ -157,13 +189,13 @@ public class SuperArray{
 	System.out.println("Printing SuperArray mayfield post-remove...");
 	System.out.println(mayfield);
 	    
-	mayfield.add(3,99);
+	mayfield.add(3,new Rational(99,100));
 	System.out.println("Printing SuperArray mayfield post-insert...");
 	System.out.println(mayfield);
-	mayfield.add(2,88);
+	mayfield.add(2,new Binary(88));
 	System.out.println("Printing SuperArray mayfield post-insert...");
 	System.out.println(mayfield);
-	mayfield.add(1,77);
+	mayfield.add(1,new Hexadecimal(77));
 	System.out.println("Printing SuperArray mayfield post-insert...");
 	System.out.println(mayfield);
 
